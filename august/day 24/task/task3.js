eba91011ca4d9f1257f7a44dacf0f85335e17198
Yousef Mohamed <ym8370302@gmail.com>
2025-09-06 19:24:41 +0300

    edit task3 for mongodb

--- august/day 24/task/task3.js
+++ august/day 24/task/task3.js
@@ -1,27 +1,27 @@
 let courses = [
-  { name: "Course 1", finalMark: 100 },
-  { name: "Course 2", finalMark: 80 },
-  { name: "Course 3", finalMark: 60 },
-  { name: "Course 4", finalMark: 50 },
-  { name: "Course 5", finalMark: 120 },
+  { name: "Data Structures & Algorithms", finalMark: 100 },
+  { name: "Operating Systems", finalMark: 80 },
+  { name: "Database Systems", finalMark: 60 },
+  { name: "Computer Networks", finalMark: 50 },
+  { name: "Software Engineering", finalMark: 120 },
 ];
 
 let faculties = [
   {
-    name: "Faculty 1",
-    address: "Address 1",
+    name: "Cairo University",
+    address: "Cairo",
   },
   {
-    name: "Faculty 2",
-    address: "Address 2",
+    name: "Alexandria University",
+    address: "Alexandria",
   },
   {
-    name: "Faculty 3",
-    address: "Address 3",
+    name: "Mansoura University",
+    address: "Mansoura",
   },
   {
-    name: "Faculty 4",
-    address: "Address 4",
+    name: "Ain Shams University",
+    address: "Cairo",
   },
 ];
 
@@ -30,14 +30,14 @@ let students = [
     firstName: "Ahmed",
     lastName: "Mohamed",
     isFired: false,
-    facultyId: ObjectId("68b80fa9144318c127f67b94"),
+    facultyId: ObjectId("68bc5c6b70653b7166e26139"),
     courses: [
       {
-        courseId: ObjectId("68b80fa9144318c127f67b9a"),
+        courseId: ObjectId("68bc5c6b70653b7166e2613d"),
         grade: 80,
       },
       {
-        courseId: ObjectId("68b80fa9144318c127f67b9b"),
+        courseId: ObjectId("68bc5c6b70653b7166e2613e"),
         grade: 60,
       },
     ],
@@ -46,20 +46,21 @@ let students = [
     firstName: "Hesham",
     lastName: "Ali",
     isFired: true,
-    facultyId: ObjectId("68b80fa9144318c127f67b95"),
+    facultyId: ObjectId("68bc5c6b70653b7166e2613b"),
     courses: [
       {
-        courseId: ObjectId("68b80fa9144318c127f67b9a"),
-        grade: 30,
+        courseId: ObjectId("68bc5c6b70653b7166e2613f"),
+        grade: 25,
       },
       {
-        courseId: ObjectId("68b80fa9144318c127f67b9b"),
+        courseId: ObjectId("68bc5c6b70653b7166e26140"),
         grade: 20,
       },
     ],
   },
 ];
 
+// 1
 use("FacultySystemV2");
 db.createCollection("student", {
   validator: {
@@ -142,15 +143,17 @@ db.course.find();
 db.student.insertMany(students);
 db.student.find();
 
+// 2
 db.student.aggregate([
   {
     $project: {
-      fullName: { $concat: ["$firstName", " ", " $lastName"] },
+      fullName: { $concat: ["$firstName", " ", "$lastName"] },
       averageGrade: { $avg: "$courses.grade" },
     },
   },
 ]);
 
+// 3
 db.course.aggregate([
   {
     $group: {
@@ -160,6 +163,7 @@ db.course.aggregate([
   },
 ]);
 
+// 4.1
 db.student.aggregate([
   {
     $match: { firstName: "Ahmed" },
@@ -174,6 +178,7 @@ db.student.aggregate([
   },
 ]);
 
+// 4.2
 db.student.aggregate([
   {
     $match: { firstName: "Ahmed" },
@@ -188,13 +193,37 @@ db.student.aggregate([
   },
 ]);
 
-// $count => counts the number of documents
+// $count => returns the number of documents that pass through the pipeline
+// we can use it after $match to count only documents that meet certain conditions
+// it outputs a single field with the name we choose like "totalStudents"
+// $sum here if we pass field path ($fieldName) it sums all
+// values of that field across documents
+// and if we pass a constant number it counts the documents in the group
+// so here {count: 1} is not a pass field nor a constant number so $sum will
+// deal with it as it's a zero and count here is not more than a name for a key
+// and it can be anything rather than count and it will still return zero
+
+db.student.aggregate([
+  { $match: { isFired: true } },
+  { $count: "firedStudents" },
+]);
+
 db.student.aggregate([
   { $unwind: "$courses" },
   {
     $group: {
       _id: "$_id",
       totalCourses: { $sum: { count: 1 } },
+    },
+  },
+]);
+
+db.student.aggregate([
+  { $unwind: "$courses" },
+  {
+    $group: {
+      _id: "$_id",
+      totalCourses: { $sum: 1 },
     },
   },
 ]);
